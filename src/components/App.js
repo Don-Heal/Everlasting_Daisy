003d0f94e6107a3015982db321b4f684a2aa3636
DonHeal <donnie.heal@gmail.com>
2022-12-10 12:18:15 +1100

    changed routing for deployment

--- src/components/App.js
+++ src/components/App.js
@@ -22,18 +22,18 @@ function App() {
         <Navigation />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/baby_products" element={<Baby />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/products/bottles" element={<Bottles />} />
-          <Route path="/products/mugs" element={<Mugs />} />
-          <Route path="/products/glasses" element={<Glasses />} />
-          <Route path="/products/giftBox" element={<Gift />} />
-          <Route path="/baby_products/babyBibs" element={<Bibs />} />
-          <Route path="/baby_products/teether" element={<Teether />} />
-          <Route path="/baby_products/dummyChains" element={<Dummy />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="baby_products" element={<Baby />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="products/bottles" element={<Bottles />} />
+          <Route path="products/mugs" element={<Mugs />} />
+          <Route path="products/glasses" element={<Glasses />} />
+          <Route path="products/giftBox" element={<Gift />} />
+          <Route path="baby_products/babyBibs" element={<Bibs />} />
+          <Route path="baby_products/teether" element={<Teether />} />
+          <Route path="baby_products/dummyChains" element={<Dummy />} />
         </Routes>
       </Router>
     </div>
