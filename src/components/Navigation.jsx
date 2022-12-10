419d6d059a463f8fe4aeb91278338d5c43d296cd
DonHeal <donnie.heal@gmail.com>
2022-12-10 12:41:19 +1100

    changed nav to use Link to over <a> tags

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,76 +1,101 @@
 import React from "react";
-import logo from "../assets/images/logo.jpg"
+import logo from "../assets/images/logo.jpg";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-
   return (
     <>
-    <div className="home">
-    <div className="home-title">
-      <img src={logo} alt="Everlasting Logo" />
-    <h1>Everlasting Daisy & Co</h1>
-    </div>
-    </div>
-    <nav class="nav">
-      <div class="nav-bar">
-      <div class="dropdown">
-          <a href="/">
-            <button class="dropbtn">Home</button>
-            </a>
-        </div>
-      <div class="dropdown">
-          <a href="/about">
-            <button class="dropbtn">About Us</button>
-            </a>
-        </div>
-        <div class="dropdown">
-          <a href="/products">
-            <button class="dropbtn">Personalised Products</button>
-            <div class="dropdown-content">
-              <a href="/products/bottles">Bottles</a>
-              <a href="/products/mugs">Mugs</a>
-              <a href="/products/glasses">Glasses</a>
-              <a href="/products/giftBox">Gift Boxes</a>
-            </div>
-          </a>
-        </div>
-        <div class="dropdown">
-          <a href="/baby_products">
-            <button class="dropbtn">Baby Products</button>
-            <div class="dropdown-content">
-              <a href="/baby_products/babyBibs">Baby Bibs</a>
-              <a href="/baby_products/teether">Baby Teethers</a>
-              <a href="/baby_products/dummyChains">Dummy Chains</a>
-            </div>
-          </a>
-        </div>
-        <div class="dropdown">
-          <a href="/orders">
-            <button class="dropbtn">Order Now</button>
-            </a>
-        </div>
-        <div class="dropdown">
-          <a href="/contact">
-            <button class="dropbtn">Contact Us</button>
-            </a>
+      <div className="home">
+        <div className="home-title">
+          <img src={logo} alt="Everlasting Logo" />
+          <h1>Everlasting Daisy & Co</h1>
         </div>
       </div>
-      <div class="hamburger-menu">
-    <input id="menu__toggle" type="checkbox" />
-    <label class="menu__btn" for="menu__toggle">
-      <span></span>
-    </label>
+      <nav class="nav">
+        <div class="nav-bar">
+          <div class="dropdown">
+            <Link to="/">
+              <button class="dropbtn">Home</button>
+            </Link>
+          </div>
+          <div class="dropdown">
+            <Link to="/about">
+              <button class="dropbtn">About Us</button>
+            </Link>
+          </div>
+          <div class="dropdown">
+            <Link to="/products">
+              <button class="dropbtn">Personalised Products</button>
+            </Link>
+            <div class="dropdown-content">
+              <Link to="/products/bottles">Bottles</Link>
+              <Link to="/products/mugs">Mugs</Link>
+              <Link to="/products/glasses">Glasses</Link>
+              <Link to="/products/giftBox">Gift Boxes</Link>
+            </div>
+          </div>
+          <div class="dropdown">
+            <Link to="/baby_products">
+              <button class="dropbtn">Baby Products</button>
+            </Link>
+            <div class="dropdown-content">
+              <Link to="/baby_products/babyBibs">Bibs</Link>
+              <Link to="/baby_products/teether">Teethers</Link>
+              <Link to="/baby_products/dummyChains">Teething Rings</Link>
+            </div>
+          </div>
+          <div class="dropdown">
+            <Link to="/orders">
+              <button class="dropbtn">Order Now</button>
+            </Link>
+          </div>
+          <div class="dropdown">
+            <Link to="/contact">
+              <button class="dropbtn">Contact Us</button>
+            </Link>
+          </div>
+        </div>
 
-    <ul class="menu__box">
-      <li><a class="menu__item" href="/">Home</a></li>
-      <li><a class="menu__item" href="/about">About Us</a></li>
-      <li><a class="menu__item" href="/products">Personalised Products</a></li>
-      <li><a class="menu__item" href="/baby_products">Baby Products</a></li>
-      <li><a class="menu__item" href="/orders">Order Now</a></li>
-      <li><a class="menu__item" href="/contact">Contact Us</a></li>
-    </ul>
-  </div>
-    </nav>
+        <div class="hamburger-menu">
+          <input id="menu__toggle" type="checkbox" />
+          <label class="menu__btn" for="menu__toggle">
+            <span></span>
+          </label>
+
+          <ul class="menu__box">
+            <li>
+              <Link to="/" class="menu__item">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" class="menu__item">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/products" class="menu__item">
+                Personalised Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/baby_products" class="menu__item">
+                Baby Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/orders" class="menu__item">
+                Order Now
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" class="menu__item">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
